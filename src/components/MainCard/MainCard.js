6d4925358d475a70279778dea270d89d23289836
Ewelina <ewela.m95@gmail.com>
2022-03-19 19:08:09 +0100

    added MUI components

--- src/components/MainCard/MainCard.js
+++ src/components/MainCard/MainCard.js
@@ -1,12 +1,15 @@
 import './MainCard.css';
 import photo from '../../assets/Food/Pierogi.jpg';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 function MainCard() {
 	return (
 		<div className='mainCard'>
 			<img className='mainCard__photo' src={photo} alt='pierogi'></img>
 			<div className='mainCard__title'>
-				<h1>Pierogi z Awganistanu</h1>
+				<h1>Pierogi Aushak</h1>
 				<h2>
 					Aushak are an Afghan version of dumplings, usually filled with leeks,
 					onions, or chives. It's served with creamy garlic yogurt and a hearty
@@ -16,24 +19,35 @@ function MainCard() {
 					prepare this lovely dish in good company.
 				</h2>
 
-				<div className='mainCard__info'>
-					<div className='mainCard__info__item'>
-						<h3>Ilość porcji</h3>
-						<p>3</p>
-					</div>
-					<div className='mainCard__info__item'>
-						<h3>Czas</h3>
-						<p>3</p>
-					</div>
-					<div className='mainCard__info__item'>
-						<h3>Przygotowanie</h3>
-						<p>3</p>
-					</div>
-					<div className='mainCard__info__item'>
-						<h3>Gotowanie</h3>
-						<p>3</p>
-					</div>
-				</div>
+				<Box className='mainCard__info'>
+					<Grid container  justifyContent="end">
+						<Grid item xs={5} sm={3}>
+						<Paper className='mainCard__info__item' elevation={4}>
+							<h5>Ilość porcji</h5>
+							<p>3</p>
+						</Paper>
+						</Grid>
+						<Grid item xs={6} sm={3}>
+						<Paper className='mainCard__info__item' elevation={4}>
+							<h5>Czas ogółem</h5>
+							<p>30 min</p>
+						</Paper>
+						</Grid>
+						<Grid item xs={5} sm={3}>
+						<Paper className='mainCard__info__item' elevation={4}>
+							<h5>Przygotowanie</h5>
+							<p>10 min</p>
+						</Paper>
+						</Grid>
+						<Grid item xs={6} sm={3}>
+						<Paper className='mainCard__info__item' elevation={4}>
+							<h5>Gotowanie</h5>
+							<p>20 min</p>
+						</Paper>
+						</Grid>
+						
+					</Grid>
+				</Box>
 			</div>
 		</div>
 	);
