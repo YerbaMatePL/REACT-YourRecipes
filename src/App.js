b519596b3c8ce1cca571660f0124465576a7d1ca
Ewelina <ewela.m95@gmail.com>
2022-03-28 12:44:22 +0200

    added darkMode

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, { useState } from 'react';
 import './App.css';
 import MainCard from './components/MainCard/MainCard';
 import Recipe from './components/Recipe/Recipe';
+import Switch from '@mui/material/Switch';
+
 
 function App() {
+	// Theme
+	const [theme, setTheme] = useState('light');
+
+	const checkTheme = () => theme === 'light' ? setTheme('dark') :setTheme('light') ;
+
+	// Amount of Portions
 	const [amountOfPortions , setAmountOfPortions] = useState(1);
 
 	const updateAmountOfPortions = (dataFromChild) => {
 		setAmountOfPortions(dataFromChild);
 	};
-
-
-
-
 	return (
-		<div className='app wrapper'>
+		<div className={`app wrapper app__bgc--${theme}`}>
+			<Switch onChange={checkTheme} defaultChecked color="default"  
+			inputProps={{ 'aria-label': 'checkbox with default color' }}/>
 			<MainCard update={updateAmountOfPortions}/>
 			<Recipe value={amountOfPortions}/>
 		</div>
+		
 	);
 }
 
