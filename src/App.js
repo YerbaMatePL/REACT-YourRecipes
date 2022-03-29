32cd4e86a008bc3c9fd1d02af43a66c67f4e082c
Ewelina <ewela.m95@gmail.com>
2022-03-29 09:21:36 +0200

    added local storage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MainCard from './components/MainCard/MainCard';
 import Recipe from './components/Recipe/Recipe';
 import Switch from '@mui/material/Switch';
 
 
-
-
 function App() {
-	// Theme
-	const [theme, setTheme] = useState('light');
+	// Theme and local storage
+	const [theme, setTheme] = useState(localStorage.getItem("theme") || 'light');
 
-	const checkTheme = () => theme === 'light' ? setTheme('dark') :setTheme('light') ;
+	useEffect(() => {
+		localStorage.setItem('theme', theme);
+	  }, [theme]);
+
+	const checkTheme = () =>
+		theme === 'light' ? setTheme('dark') : setTheme('light');
 
 	// Amount of Portions
-	const [amountOfPortions , setAmountOfPortions] = useState(1);
+	const [amountOfPortions, setAmountOfPortions] = useState(1);
 
 	const updateAmountOfPortions = (dataFromChild) => {
 		setAmountOfPortions(dataFromChild);
 	};
 
-	
 	return (
 		<div className={`app wrapper app__bgc--${theme}`}>
-			<Switch onChange={checkTheme} color="primary"/>
-			<MainCard update={updateAmountOfPortions}/>
-			<Recipe value={amountOfPortions}/>
+			<Switch onChange={checkTheme} color='primary' />
+			<MainCard update={updateAmountOfPortions} />
+			<Recipe value={amountOfPortions} />
 		</div>
-		
 	);
 }
 
